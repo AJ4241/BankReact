9c41041ca8a810aa0fd8239e9defa102aa9a1e4a
a20j30 <42009944+AJ4241@users.noreply.github.com>
2021-11-21 21:42:42 -0500

    Completed code with comments

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,9 @@ class Home extends Component {
     return (
       <div>
         <h1>Bank of React!</h1>
+{/*
+  I am using Link to allow the user to click on it and transfer to another page
+*/}
         <Link to="/userProfile">User Profile  </Link>
         <Link to="/Login">Log In  </Link>
         <Link to="/Debits">Debits  </Link>
